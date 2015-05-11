f0db5e759c607b1b0d804add4a1febc013e531f4
Amigirl55 <adrenalineac@gmail.com>
2015-05-10 23:10:55 -0400

    Committed by Udacity

---
+++ generatehtml.py
@@ -0,0 +1,39 @@
+Python 2.7.9 (v2.7.9:648dcafa7e5f, Dec 10 2014, 10:10:46) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+>>> def generate_concept_HTML(concept_title, concept_description):
+    html_text_1 = '''
+<div class="concept">
+    <div class="concept-title">
+        ''' + concept_title
+    html_text_2 = '''
+    </div>
+    <div class="concept-description">
+        ''' + concept_description
+    html_text_3 = '''
+    </div>
+</div>'''
+    full_html_text = html_text_1 + html_text_2 + html_text_3
+    return full_html_text
+
+def make_HTML(concept):
+    concept_title = concept[0]
+    concept_description = concept[1]
+    return generate_concept_HTML(concept_title, concept_description)
+
+EXAMPLE_LIST_OF_CONCEPTS = [ ['Python', 'Python is a Programming Language'],
+                             ['For Loop', 'For Loops allow you to iterate over lists'],
+                             ['Lists', 'Lists are sequences of data'] ]
+
+
+def make_HTML_for_many_concepts(list_of_concepts):
+    HTML = ""
+    for concept in list_of_concepts:
+        concept_HTML = make_HTML(concept)
+        HTML = HTML + concept_HTML
+    return HTML
+
+print make_HTML_for_many_concepts(EXAMPLE_LIST_OF_CONCEPTS)
